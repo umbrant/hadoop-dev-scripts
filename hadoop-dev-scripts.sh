06eb1b46a9540fe80125148bd2211a7619da4c21
Andrew Wang <andrew.wang@cloudera.com>
2015-02-24 11:23:49 -0800

    Improve help text, make source script work with zsh.

--- hadoop-dev-scripts.sh
+++ hadoop-dev-scripts.sh
@@ -1,12 +1,16 @@
-#!/bin/bash
+#!/bin/sh
 
 # Source this file to make all the scripts available on your PATH
 
 # Location of this file
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DIR="$( cd "$( dirname "$0" )" && pwd )"
 
 # Set up the PATH relative to the base DIR
+echo $PATH
 export PATH=$PATH:$DIR/
 export PATH=$PATH:$DIR/git/
 export PATH=$PATH:$DIR/build-and-run/
 export PATH=$PATH:$DIR/tests/
+echo $PATH
+
+unset DIR
