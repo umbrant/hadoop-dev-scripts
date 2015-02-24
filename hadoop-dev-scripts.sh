66fde2ed7a870047963d242ca196c6a6d1f6b004
Andrew Wang <andrew.wang@cloudera.com>
2015-02-24 11:15:59 -0800

    Copying much of this over from my dotfiles repo.

---
+++ hadoop-dev-scripts.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Source this file to make all the scripts available on your PATH
+
+# Location of this file
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Set up the PATH relative to the base DIR
+export PATH=$PATH:$DIR/
+export PATH=$PATH:$DIR/git/
+export PATH=$PATH:$DIR/build-and-run/
+export PATH=$PATH:$DIR/tests/
